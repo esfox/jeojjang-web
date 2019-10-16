0214f106fc4a58750c8b97219b4f3649b903840a
esfox <osairis100@gmail.com>
2019-10-16 17:25:31 +0800

    Test commit for github pages

---
+++ docs/precache-manifest.70441bfa4e9bab5cec2eba8cfcf88cf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa0b42e9dd2d24ff7e424afe47b79db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88fa2ee984739390d00",
+    "url": "/static/css/main.501f6533.chunk.css"
+  },
+  {
+    "revision": "e9801a999a1e02c6520b",
+    "url": "/static/js/2.1bf13495.chunk.js"
+  },
+  {
+    "revision": "e88fa2ee984739390d00",
+    "url": "/static/js/main.0ef7efc6.chunk.js"
+  },
+  {
+    "revision": "bd5cdfb8230f3c69be2c",
+    "url": "/static/js/runtime~main.b3e77ead.js"
+  }
+]);
